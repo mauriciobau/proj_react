6c8571b1e833374b022092d24ae996f5d10bf9b1
Maurício A. Baú <mauricio.a.bau@gmail.com>
2019-11-05 15:14:57 -0300

    Listando repositórios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,7 @@ import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
@@ -38,7 +38,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { newRepo, loading } = this.state;
+    const { newRepo, respositories, loading } = this.state;
 
     return (
       <Container>
@@ -63,6 +63,15 @@ export default class Main extends Component {
             )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {respositories.map(respository => (
+            <li key={respository.name}>
+              <span>{respository.name}</span>
+              <a href="">Detalhes</a>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
